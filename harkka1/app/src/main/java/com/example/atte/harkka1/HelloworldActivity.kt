d56ec35cbd7d630ce404e4b6aca61c0b104b4bb8
keijjO <haapanen.atte@gmail.com>
2018-02-01 11:14:37 +0200

    Hello worldi

---
+++ harkka1/app/src/main/java/com/example/atte/harkka1/HelloworldActivity.kt
@@ -0,0 +1,12 @@
+package com.example.atte.harkka1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HelloworldActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.helloworld)
+    }
+}
